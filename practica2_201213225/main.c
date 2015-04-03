13a2f5bf62773dbd406bdcf6716dafe8bcdeb945
201213225 <kandor95@gmail.com>
2015-04-03 15:46:18 -0600

    Correjir

---
+++ practica2_201213225/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: alex
+ *
+ * Created on 3 de abril de 2015, 15:45
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
