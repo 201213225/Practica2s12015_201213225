0ad24c54d54ff086bf2713922e3525456279f403
201213225 <kandor95@gmail.com>
2015-04-04 08:00:55 -0600

    Nodo

--- practica2_201213225/nodo.c
+++ practica2_201213225/nodo.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "nodo.h"
+Nodo *incertar_nodo(Nodo *raiz, int info){
+    if(raiz->info == 0){
+        raiz->info = info;
+        raiz->der = malloc(sizeof(Nodo));
+        raiz->izq = malloc(sizeof(Nodo));
+    }else{
+        if(raiz->info < info){
+            raiz->der = incertar(raiz->der, info);
+        }else if(raiz->info > info){
+            raiz->izq = incertar(raiz->izq, info);
+        }
+    }
+    return raiz;
+}
